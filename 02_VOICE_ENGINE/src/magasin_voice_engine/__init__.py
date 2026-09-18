f40cd2328bda8eb2868bceabf358bbee27a0c6cc
MAGASIN <bachvantoi1994@gmail.com>
2026-09-18 10:36:12 +0700

    feat(saydi): export supervisor status API

--- 02_VOICE_ENGINE/src/magasin_voice_engine/__init__.py
+++ 02_VOICE_ENGINE/src/magasin_voice_engine/__init__.py
@@ -10,18 +10,30 @@ from .models import (
 )
 from .presets import PRESETS, VoicePreset, get_preset
 from .provider import SaydiVoiceProvider
+from .supervisor_status import (
+    HeartbeatWorker,
+    RobotStatus,
+    SupervisorState,
+    SupervisorStateStore,
+    default_job_id,
+)
 
 __all__ = [
     "AppliedSettings",
     "DownloadReceipt",
     "FailureDisposition",
     "GenerationReceipt",
+    "HeartbeatWorker",
     "PRESETS",
     "PreflightResult",
+    "RobotStatus",
     "RunState",
     "SaydiVoiceProvider",
+    "SupervisorState",
+    "SupervisorStateStore",
     "VoicePreset",
     "VoiceRequest",
     "VoiceResult",
+    "default_job_id",
     "get_preset",
 ]
